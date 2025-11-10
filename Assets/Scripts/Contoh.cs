5126d283bfefb4d38416cd7c1b4bebe03ffe1df4
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-10 17:06:04 +0700

    belajar fundamental Unity

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -1,16 +1,30 @@
+using System.Globalization;
+using Mono.Cecil.Cil;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Contoh : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int nyawa = 10;   // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log ("Perkalian (5,5,5) = " + Kali(5,5,5));
+    }   
+    public void MinumObat()
+    {
+        Debug.Log("Pemain meminum obat");
+        Sembuhkan(30);
+
+    }
+    public void Sembuhkan(int tambahanNyawa)
+    {
+        nyawa = nyawa + tambahanNyawa;
+
+    }
+    public int Kali(int angka1, int angka2, int angka3)
+    {
+        int result= angka1* angka2* angka3;
+        return result;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
