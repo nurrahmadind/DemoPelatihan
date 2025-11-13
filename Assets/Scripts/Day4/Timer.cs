5526e6204f2c855275069091d1b276baa748d685
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-13 16:04:30 +0700

    fill the object
    kayak membuat timer atau jam pasir

--- Assets/Scripts/Day4/Timer.cs
+++ Assets/Scripts/Day4/Timer.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public int waktu;
+    public float waktu;
+    
+    public int MaximumWaktu;
     public TMP_Text  TextTimer;
     public bool WaktuBerjalan;
 
@@ -15,27 +17,34 @@ public class Timer : MonoBehaviour
     public KeyCode StopCoroutineKey;
 
     public Coroutine HitungTimerCoroutine;
+
+    public Image ProgressFill;
     // Update is called once per frame
     private void Update()
     {
         if(Input.GetKeyDown(StartCoroutineKey))
         {
-            HitungTimerCoroutine=StartCoroutine(HitungTimer());
+            if (HitungTimerCoroutine==null)
+            {
+            HitungTimerCoroutine=StartCoroutine(HitungTimer()); 
+            }
         }
         if(Input.GetKeyDown(StopCoroutineKey))
         {
             if (HitungTimerCoroutine !=null)
             {
                  StopCoroutine(HitungTimerCoroutine);
+                 HitungTimerCoroutine = null; //balik ke 0, atau permainan awal
             }
         }
     }
     IEnumerator HitungTimer()
     {
-        while (WaktuBerjalan == true)
+        while (WaktuBerjalan == true && waktu < MaximumWaktu)
         {
         waktu = waktu +1;
         TextTimer.text = waktu.ToString();
+        ProgressFill.fillAmount = waktu/ MaximumWaktu;
         yield return new WaitForSeconds(1);
         }
     }
