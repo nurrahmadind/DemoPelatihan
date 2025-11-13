f7eae69f9c35134a9f4052a6ad198e05ac0bf249
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-13 14:47:34 +0700

    timer.cs

--- Assets/Scripts/Day4/Timer.cs
+++ Assets/Scripts/Day4/Timer.cs
@@ -1,16 +1,42 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Timer : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int waktu;
+    public TMP_Text  TextTimer;
+    public bool WaktuBerjalan;
 
+    public KeyCode StartCoroutineKey;
+
+    public KeyCode StopCoroutineKey;
+
+    public Coroutine HitungTimerCoroutine;
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(StartCoroutineKey))
+        {
+            HitungTimerCoroutine=StartCoroutine(HitungTimer());
+        }
+        if(Input.GetKeyDown(StopCoroutineKey))
+        {
+            if (HitungTimerCoroutine !=null)
+            {
+                 StopCoroutine(HitungTimerCoroutine);
+            }
+        }
+    }
+    IEnumerator HitungTimer()
+    {
+        while (WaktuBerjalan == true)
+        {
+        waktu = waktu +1;
+        TextTimer.text = waktu.ToString();
+        yield return new WaitForSeconds(1);
+        }
     }
 }
